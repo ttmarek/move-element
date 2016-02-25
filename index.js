090b5b139d893ea2650c68aac4958f21506802c8
ttmarek <ttmarek@outlook.com>
2016-02-24 21:29:51 -0700

    Function passes all tests

--- index.js
+++ index.js
@@ -2,8 +2,33 @@
 
 module.exports = moveElement
 
-function moveElement(arr, check, index) {
-
-
-
+/**
+ *
+ * @param {Array} arr
+ * @param {Function} isElement
+ * @param {Integer} index
+ */
+function moveElement(arr, isElement, index) {
+  var maxIndex = arr.length - 1
+  // Return the original array if the given index isn't valid
+  if (index > maxIndex || index < 0) {
+    return arr
+  }
+  // Find the element's current index in the array
+  var currentIndex = (function () {
+    for (var i = 0; i <= maxIndex; i++) {
+      if (isElement(arr[i])) {
+        return i
+      }
+    }
+    return -1
+  })()
+  // If the element isn't in the array return the original
+  if (currentIndex === -1) {
+    return arr
+  }
+  var arrCopy = arr.slice()                        // make a copy
+  var element = arrCopy.splice(currentIndex, 1)[0] // pop the element out
+  arrCopy.splice(index, 0, element)                // put the element back at index
+  return arrCopy
 }
